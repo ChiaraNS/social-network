9460d845d9ee9cd87722f177a081f4447a56df99
Key <cns3.22202@gmail.com>
2022-01-27 20:53:21 -0600

    thought routes

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,30 @@
+const router = require('express').Router();
+
+const {
+    getAllThoughts,
+    getThoughtById,
+    createThought,
+    updateThought,
+    deleteThought,
+    createReaction,
+    deleteReaction,
+} = require('../../controllers/thought-control');
+
+router.route('/')
+.get(getAllThoughts);
+
+router.route('/:userId')
+.post(createThought);
+
+router.route('/:thoughtId')
+.get(getThoughtById)
+.put(updateThought)
+.delete(deleteThought)
+
+router.route('/:thoughtId/reactions')
+.post(createReaction);
+
+router.route('/:thoughtId/reactions/:reactionId')
+.delete(deleteReaction);
+
+module.exports = router;
